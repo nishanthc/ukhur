f3bbbf7dac9802ef18b1b1ad4986d4b38421cee5
nishanthc <nishanthchandradas@gmail.com>
2020-06-11 00:35:47 +0100

    added models

--- analyse/models.py
+++ analyse/models.py
@@ -1,3 +1,26 @@
+import uuid as uuid
+from django.contrib.postgres.fields import JSONField
 from django.db import models
 
+
 # Create your models here.
+
+
+class Report(models.Model):
+    uuid = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    word_occurrences_count = JSONField()
+
+
+class Document(models.Model):
+    text = models.TextField(
+        blank=True,
+        null=True
+    )
+
+    word_occurrences_count = JSONField()
+
+    word_occurrences_sentence = JSONField()

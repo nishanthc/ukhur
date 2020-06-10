a0d7150aad5f6fb30cd0247292386d2d857fa97d
nishanthc <nishanthchandradas@gmail.com>
2020-06-10 12:45:20 +0100

    Added base templates

--- frontend/views.py
+++ frontend/views.py
@@ -7,13 +7,9 @@ from django.views.generic import TemplateView
 class HomeView(TemplateView):
     template_name = "homepage.html"
 
-    # def get_context_data(self, *args, **kwargs):
-    #     context = super(HomeView, self).get_context_data(*args, **kwargs)
-    #     plans = Product.objects.filter(is_addon=False).values('id', 'name', 'liquid_quantity')
-    #     plans = [entry for entry in plans]
-    #     context['plans'] = plans
-    #     faqs = Faq.objects.values('question', 'answer')
-    #     faqs = [entry for entry in faqs]
-    #     context['faqs'] = faqs
-    #
-    #     return context
+    def get_context_data(self, *args, **kwargs):
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+
+        context['website_name'] = "Ukhur"
+
+        return context

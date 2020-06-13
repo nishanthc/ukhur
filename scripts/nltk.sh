8c73a068aea64f025d5ec8ea22ef15b311c9953e
nishanthc <nishanthchandradas@gmail.com>
2020-06-13 15:03:20 +0100

    eb

--- scripts/nltk.sh
+++ scripts/nltk.sh
@@ -1,4 +1,4 @@
-source /opt/python/run/venv/bin/activate
-export NLTK_DATA="/home/ec2-user/nltk_data/"
-pip3 install nltk
-python scripts/nltk_download.py
+#source /opt/python/run/venv/bin/activate
+#export NLTK_DATA="/home/ec2-user/nltk_data/"
+#pip3 install nltk
+#python scripts/nltk_download.py
